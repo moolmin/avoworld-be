9be56ed6e3568a2b57177952890e9a0431a7d1b7
moolmin <zzmzzm0065@gmail.com>
2024-06-29 04:58:55 +0900

    fix workflow

--- src/main/java/com/avoworld/env/config/PropertyConfig.java
+++ src/main/java/com/avoworld/env/config/PropertyConfig.java
@@ -1,14 +1,14 @@
-package com.avoworld.env.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-
-@Configuration
-@PropertySources({
-        @PropertySource("classpath:property/env.properties")
-})
-
-public class PropertyConfig {
-
-}
+//package com.avoworld.env.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.context.annotation.PropertySources;
+//
+//@Configuration
+//@PropertySources({
+//        @PropertySource("classpath:property/env.properties")
+//})
+//
+//public class PropertyConfig {
+//
+//}
