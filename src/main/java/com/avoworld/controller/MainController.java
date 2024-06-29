9c8023cb04bc0dd2a168c108aaff5e3ce21c205c
moolmin <zzmzzm0065@gmail.com>
2024-06-30 02:55:24 +0900

    edit main controller

--- src/main/java/com/avoworld/controller/MainController.java
+++ src/main/java/com/avoworld/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.avoworld.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 
     @GetMapping("/")
-    public String mainP() {
+    public String main() {
         return "실뱌의 avoworld를 위한 서버 엔드포인트입니다 0630.";
     }
 }
