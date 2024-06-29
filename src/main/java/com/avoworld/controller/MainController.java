a958219ed0fe3a31d10df03ac68a4bf44688a50a
moolmin <zzmzzm0065@gmail.com>
2024-06-30 02:47:49 +0900

    edit cors again

--- src/main/java/com/avoworld/controller/MainController.java
+++ src/main/java/com/avoworld/controller/MainController.java
@@ -11,6 +11,6 @@ public class MainController {
 
     @GetMapping("/")
     public String mainP() {
-        return "실뱌의 avoworld를 위한 서버 엔드포인트입니다.";
+        return "실뱌의 avoworld를 위한 서버 엔드포인트입니다 0630.";
     }
 }
