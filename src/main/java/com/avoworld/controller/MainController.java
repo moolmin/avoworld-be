e29b604a17de9c1f5e8205019e0ce6c07f5141ef
moolmin <zzmzzm0065@gmail.com>
2024-07-01 00:30:24 +0900

    fix main controller

--- src/main/java/com/avoworld/controller/MainController.java
+++ src/main/java/com/avoworld/controller/MainController.java
@@ -2,12 +2,14 @@ package com.avoworld.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class MainController {
 
     @GetMapping("/")
     public String main() {
-        return "index";
+        return "실뱌의 avoworld를 위한 서버 엔드포인트입니다.";
     }
 }
