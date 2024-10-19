5c0a96004a5d4fc1d2f3ed74eda78af281a9951a
Sumin Kim <zzmzzm0065@gmail.com>
2024-10-19 13:44:40 +0900

    fix: import AntPathMatcher in jwt filter

--- src/main/java/com/avoworld/jwt/JWTFilter.java
+++ src/main/java/com/avoworld/jwt/JWTFilter.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.util.AntPathMatcher;
 
 import java.io.IOException;
 
