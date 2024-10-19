44ba539eaf52e231eb7ee475b12819fe5a950789
Sumin Kim <zzmzzm0065@gmail.com>
2024-10-19 13:40:27 +0900

    feat: add should not filter to get posts without token

--- src/main/java/com/avoworld/jwt/JWTFilter.java
+++ src/main/java/com/avoworld/jwt/JWTFilter.java
@@ -16,7 +16,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-// 요청에 대해 한번만 동작하는 필터
 public class JWTFilter extends OncePerRequestFilter {
 
     private final JWTUtil jwtUtil;
@@ -26,62 +25,47 @@ public class JWTFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String requestURI = request.getRequestURI();
 
-        // 회원가입 요청에는 토큰 요청 제외
-        if ("/api/join".equals(requestURI) || "/api/accounts/check-email".equals(requestURI) || requestURI.startsWith("/uploads/")) {
-            System.out.println("Bypassing JWT filter for: " + requestURI); // Log the bypassing
-            filterChain.doFilter(request, response);
-            return;
-        }
+        AntPathMatcher pathMatcher = new AntPathMatcher();
 
-        //request에서 Authorization 헤더 찾기
+        return pathMatcher.match("/api/join", requestURI) ||
+               pathMatcher.match("/api/accounts/check-email", requestURI) ||
+               pathMatcher.match("/uploads/**", requestURI) ||
+               pathMatcher.match("/api/posts/**", requestURI); 
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
         String authorization = request.getHeader("Authorization");
 
-        // Authorization 헤더 검증
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-//            System.out.println("token null or does not start with Bearer");
             filterChain.doFilter(request, response);
-
-            // 조건 해당되면 메소드 종료
             return;
         }
 
-        // 이제 토큰에서 정보 떼오기
         String token = authorization.split(" ")[1];
 
-        // 토큰 소멸 시간 검증
         if (jwtUtil.isExpired(token)) {
             System.out.println("token expired");
-            // filterChain으로 방금 받은 요청, 응답 토스
             filterChain.doFilter(request, response);
-
-            // 조건 해당되면 메소드 종료
             return;
         }
 
-        // 위에서 두개의 조건문을 통해 토큰을 확인함
-        // 토큰에서 이메일 획득
         String email = jwtUtil.getEmail(token);
-//        System.out.println("User email from token: " + email);
 
-        //userEntity를 생성해서 값 set
         User user = new User();
         user.setEmail(email);
         user.setPassword("temppassword");
 
-        //userDetails에 회원 정보 객체 담기
         CustomUserDetails customUserDetails = new CustomUserDetails(user);
 
-        //스프링 시큐리티 인증 토큰 생성
         Authentication authToken  = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
 
-        //세션에 사용자 등록
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
-        //다음 필터로 요청, 응답한거 넘겨줌
         filterChain.doFilter(request, response);
     }
 }
